24c3abbabb126e4ed25134e9bced21d6b3a4a5b5
Mastertech98 <williamhadiman@gmail.com>
2018-11-23 11:51:07 +0700

    result query in JSON

--- Node/db_query.js
+++ Node/db_query.js
@@ -4,12 +4,7 @@ var queryResult = function getQueryResult(sql,callback){
     db.connect();
     db.query(sql,function (err, result) {
         if (err) throw err;
-        // result.forEach(hasil => {
-        //     //console.log(hasil);
-        //     //data_Result.push(hasil);
-        //     //console.log(data_Result);
-        // });
-        callback(result);
+        callback(JSON.stringify(result));
     });
     db.end();
 };
--- Node/db_try.js
+++ Node/db_try.js
@@ -1,4 +1,4 @@
 var db_query = require("./db_query.js");
-db_query("SELECT * FROM nasabah WHERE Nama = 'William Juniarta Hadiman'",function(data){
+db_query("SELECT * FROM nasabah",function(data){
     console.log(data);
 });
