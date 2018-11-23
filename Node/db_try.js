6d0238ea2854042ca8c035e5137735090d175fd6
shevalda <nightlyfurious@gmail.com>
2018-11-23 18:33:54 +0700

    Adding route for card checking (bank webservice)
    + express module added
    + server for bank web service added
    + db_query reverted (returns array, not in json string)

--- Node/db_try.js
+++ Node/db_try.js
@@ -2,6 +2,7 @@ var db_query = require("./db_query.js");
 // db_query("INSERT INTO `nasabah` (`Nama`, `No_Kartu`, `Saldo`) VALUES ('Dicky Adrian', '102938475647', '15000000');",function(data){
 //     console.log('Nasabah berhasil di update');
 // });
-db_query("SELECT * FROM nasabah",function(data){
+db_query("SELECT count(Nama) AS count_card FROM nasabah WHERE No_Kartu = 121234345656 GROUP BY No_Kartu",
+     function(data){
      console.log(data);
 });
