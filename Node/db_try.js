8d62237d18e609a841ace783c3a93418e08c9843
Mastertech98 <williamhadiman@gmail.com>
2018-11-23 12:11:53 +0700

    try insert to table

--- Node/db_try.js
+++ Node/db_try.js
@@ -1,4 +1,7 @@
 var db_query = require("./db_query.js");
+// db_query("INSERT INTO `nasabah` (`Nama`, `No_Kartu`, `Saldo`) VALUES ('Dicky Adrian', '102938475647', '15000000');",function(data){
+//     console.log('Nasabah berhasil di update');
+// });
 db_query("SELECT * FROM nasabah",function(data){
-    console.log(data);
+     console.log(data);
 });
