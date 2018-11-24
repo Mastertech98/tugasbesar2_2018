477ff0655c0badafff7d578c9b2666be12bd6e5d
Mastertech98 <williamhadiman@gmail.com>
2018-11-24 20:32:02 +0700

    check

--- Node/db_try.js
+++ Node/db_try.js
@@ -1,7 +1,9 @@
-var db_query = require("./db_query.js");
-// db_query("INSERT INTO `nasabah` (`Nama`, `No_Kartu`, `Saldo`) VALUES ('Dicky Adrian', '102938475647', '15000000');",function(data){
-//     console.log('Nasabah berhasil di update');
-// });
-db_query("SELECT * FROM nasabah",function(data){
-     console.log(data);
+var db_query1 = require("./db_query.js");
+//var db_query2 = require("./db_query.js");
+db_query1("INSERT INTO `nasabah` (`Nama`, `No_Kartu`, `Saldo`) VALUES ('Dicky Adrian', '102938475647', '15000000'); SELECT * FROM `nasabah`;",function(data){
+    console.log('Nasabah berhasil di update');
 });
+
+// db_query1("SELECT * FROM nasabah;",function(data){
+//      console.log(data);
+// });
