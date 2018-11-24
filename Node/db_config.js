75b1be87259d1b5f34a444b1954ab2de3c1184cc
Mastertech98 <williamhadiman@gmail.com>
2018-11-24 21:20:58 +0700

    fix multiple query error

--- Node/db_config.js
+++ Node/db_config.js
@@ -1,10 +1,20 @@
 var mysql = require('mysql');
 
-var db = mysql.createConnection({
-    host: "localhost",
-    user: "root",
-    password: "",
-    database: "bank_webservice"
-});
+var queryResult = function getQueryResult(sql,callback){
+    var db = mysql.createConnection({
+        host: "localhost",
+        user: "root",
+        password: "",
+        database: "bank_webservice"
+    });
 
-module.exports = db;
+    db.connect(function(err){
+        db.query(sql,function (err, result) {
+            if (err) throw err;
+            callback(result);
+            db.end(); 
+        });
+    });
+};
+
+module.exports = queryResult;
