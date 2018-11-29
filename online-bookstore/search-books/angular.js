329126db8b485ba996a4fc699f30278719b4426a
dickyadrian <13516050@std.stei.itb.ac.id>
2018-11-29 10:38:48 +0700

    searchbooks and result in one, angular done

---
+++ online-bookstore/search-books/angular.js
@@ -0,0 +1,23 @@
+var app = angular.module("myapp", [])
+app.controller("mycontroller", function($scope, $http){
+    $scope.showresultcount = false;
+    $scope.showloading = false;
+    $scope.error = false;
+    $scope.search = function(){
+        $scope.showloading = true;
+        $scope.showresultcount = false;
+        $scope.showresult = false;
+        $scope.error = false;
+        $http.post(
+            "search-result.php",
+            {'title':$scope.title}
+        ).then(function(data){
+            console.log(data.data.length);
+            $scope.showloading = false;
+            $scope.showresultcount = true;
+            $scope.showresult = true;
+            $scope.results = data.data;
+            $scope.error = !(data.data.length);
+        })
+    }
+});
